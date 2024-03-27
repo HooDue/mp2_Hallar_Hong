e06cdba3da2a244dd63b3674e229815e6e0486e4
HooDue <letty_hong@dlsu.edu.ph>
2024-03-28 00:11:02 +0800

    no print, and Z is idk

--- mp2_Hallar_Hong/asmfunc.asm
+++ mp2_Hallar_Hong/asmfunc.asm
@@ -1,17 +1,34 @@
 ;assemly part using x86-64
-
 section .data
-msg db "Hello world", 13, 10, 0
 section .text
 bits 64
 default rel
-global asmhello
-extern printf
+global daxpy
 
-asmhello:
-	sub rsp, 8*5
-	lea rcx, [msg]
-	call printf
-	add rsp, 8*5
+;A in rdi
+;X in rsi
+;Y in rdx
+;Z in rcx
+;size in r8
+
+daxpy:
+	movsd xmm0, [rdi]		;scalar A
+	mov eax, 0
+
+L1:
+	movsd xmm1, [rsi+8*rax]   ;X[i]
+	mulsd xmm1, xmm0		  ;A*X[i]
+	movsd xmm2, [rdx+8*rax]	  ;Y[i]
+	vaddsd xmm3, xmm1, xmm2	  ; A*X[i] + Y[i]
+	movsd [rcx+8*rax], xmm3
+	xorps xmm3, xmm3
+
+	inc eax
+	cmp eax, r8d
+	jne L1
+	jmp END
+
+END:
+	nop
 	ret
 
--- mp2_Hallar_Hong/main.c
+++ mp2_Hallar_Hong/main.c
@@ -1,3 +1,9 @@
+//FOR 2^20, 2^24 and 2^30
+#define TEN 1024
+#define TWENTY 1048576
+#define TWO_FOUR 16777216
+#define THIRTY 1073741824
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <Windows.h>
@@ -5,13 +11,12 @@
 #include <math.h>
 
 //externs
-extern void asmhello();
-extern void DAXPY(int size, double A, double* X, double* Y, double* Z);
+extern void daxpy(double A, double* X, double* Y, double* Z, int size);
 
 
-// for generating random numbers
-void initialize_X_Y(double *vector, int size) {
-    srand(time(NULL)); //seed
+// for generating random numbers, only from 0.0 to 100.0 and only one decimal place
+void initialize_X_Y(double *vector, int size, unsigned int seed) {
+    srand(seed); //seed
     for (int i = 0; i < size; i++) {
         double random_value = (double)rand() / RAND_MAX * 100.0;
 
@@ -26,26 +31,35 @@ void initialize_X_Y(double *vector, int size) {
         }
 
         vector[i] = random_value;
-        printf("%.1f\n", vector[i]);
     }
 }
 
-void DAXPY(int size, double A, double *X, double *Y, double *Z) {
-	for (int i = 0; i < size; i++) {
-		Z[i] = A * X[i] + Y[i];
-	}
+/*
+void DAXPY(double A, double* X, double* Y, double* Z, int size) {
+    for (int i = 0; i < size; i++) {
+        Z[i] = A * X[i] + Y[i];
+    }
 }
+*/
+/*
+void printZ(double A, double* X, double* Y, double* Z, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d: %f * %f * %f = %f/n", size, A, X[i], Y[i], Z[i]);
+    }
+}
+*/
+
 
 
 int main() {
-    int size = 1024;
+    int size = TEN;
 	double A = 2.0;
-	double X[size], Y[size];
-	double Z[size];
+	double X[TEN], Y[TEN];
+	double Z[TEN];
 
-	initialize_X_Y(X, size);
-    initialize_X_Y(Y, size);
-
-
+	initialize_X_Y(X, size, (unsigned int)time(NULL));
+    initialize_X_Y(Y, size, (unsigned int)time(NULL)+1);
+    daxpy(A, X, Y, Z, TEN);
+    //printZ(A, X, Y, Z, TEN);
 	return 0;
 }
