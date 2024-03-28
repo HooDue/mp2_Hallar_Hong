c0a99c245bc582f662d639fb98ca6fbc83492243
HooDue <letty_hong@dlsu.edu.ph>
2024-03-28 23:05:42 +0800

    latest

--- mp2_Hallar_Hong/main.c
+++ mp2_Hallar_Hong/main.c
@@ -15,7 +15,7 @@ extern void daxpy(double A, double* X, double* Y, double* Z, int size);
 
 
 // for generating random numbers, only from 0.0 to 100.0 and only one decimal place
-void initialize_X_Y(double *vector, int size, unsigned int seed) {
+void initialize_X_Y(double* vector, int size, unsigned int seed) {
     srand(seed); //seed
     for (int i = 0; i < size; i++) {
         double random_value = (double)rand() / RAND_MAX * 100.0;
@@ -42,17 +42,17 @@ void printZ(double A, double* X, double* Y, double* Z, int size) {
 
 int main() {
     int size = TEN;
-	double A = 2.0;
-	double X[TEN], Y[TEN];
-	double Z[TEN];
+    double A = 2.0;
+    double X[TEN], Y[TEN];
+    double Z[TEN];
 
-	initialize_X_Y(X, size, (unsigned int)time(NULL));
-    initialize_X_Y(Y, size, (unsigned int)time(NULL)+1);
+    initialize_X_Y(X, size, (unsigned int)time(NULL));
+    initialize_X_Y(Y, size, (unsigned int)time(NULL) + 1);
     daxpy(A, X, Y, Z, TEN);
-    
+
     for (int i = 0; i < size; i++) {
-        printf("%d: %.1f * %.1f + %.1f = %.1f\n", i,A,X[i],Y[i],Z[i]);
+        printf("%d: %.1f * %.1f + %.1f = %.1f\n", i, A, X[i], Y[i], Z[i]);
     }
 
-	return 0;
+    return 0;
 }
